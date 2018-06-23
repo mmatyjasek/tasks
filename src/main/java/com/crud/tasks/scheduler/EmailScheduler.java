de349c1c5478993d5f23da7319558682c17eaa6d
mmatyjasek <matyjasek.marcin@gmail.com>
2018-06-23 11:55:18 +0200

    Task 19.4 update

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -22,14 +22,11 @@ public class EmailScheduler {
     @Autowired
     AdminConfig adminConfig;
 
-    //@Scheduled(cron = "0 0 10 * * *")
-    @Scheduled(fixedDelay = 10000)
+    @Scheduled(cron = "0 0 10 * * *")
+    //@Scheduled(fixedDelay = 10000)
     public void sendInformationEmail() {
         long size = taskRepository.count();
         String task = (size != 1) ? "tasks" : "task";
-        System.out.println("\n\n\n");
-        System.out.println(task);
-        System.out.println("\n\n\n");
         emailService.send(new Mail(
                 adminConfig.getAdminMail(),
                 SUBJECT,
