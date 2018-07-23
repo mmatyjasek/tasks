b8e8ee69c2bd7719fbc3cdf6579d193b73279a8e
mmatyjasek <matyjasek.marcin@gmail.com>
2018-07-23 21:03:17 +0200

    Task 24.3 - training

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -18,16 +21,26 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/crud" );
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
+        /*context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("company", adminConfig.getCompanyName());
         context.setVariable("city", adminConfig.getCompanyCity());
         context.setVariable("country", adminConfig.getCompanyCountry());
         context.setVariable("companyEmail", adminConfig.getCompanyEmail());
-        context.setVariable("companyPhone", adminConfig.getCompanyPhone());
+        context.setVariable("companyPhone", adminConfig.getCompanyPhone());*/
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
 
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
